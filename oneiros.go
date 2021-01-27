fff4a8eec7a3e348b279ecc11575eaa69ed63da2
Sandy Urazayev <ctu@ku.edu>
2021-01-27 15:41:51 -0600

    It's ready
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

--- oneiros.go
+++ oneiros.go
@@ -1,10 +1,19 @@
-package main
+package oneiros
 
-import "fmt"
+import (
+	"crypto/rand"
+	"io"
+	"math/big"
+)
 
-func main() {
-	fmt.Println(RandInt10Times())
-	fmt.Println(RegexGenerate())
-	fmt.Println(CryptoRand())
-	fmt.Println(CrytoRandMath())
+func Generate() string {
+	n := new(big.Int)
+	bytes := make([]byte, 7)
+	for {
+		io.ReadFull(rand.Reader, bytes)
+		n.SetBytes(bytes)
+		if len(n.String()) == 16 {
+			return n.String()
+		}
+	}
 }
---
+++ oneiros_test.go
@@ -0,0 +1,9 @@
+package oneiros
+
+import "testing"
+
+func BenchmarkGenerate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Generate()
+	}
+}
